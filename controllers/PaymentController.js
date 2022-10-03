aa9785c78a1853413386905653562841d8c9b53c
David Manzano <dmanzanod@gmail.com>
2022-10-03 13:26:11 -0300

    primer commit

---
+++ controllers/PaymentController.js
@@ -0,0 +1,41 @@
+class PaymentController {
+  constructor(subscriptionService) {
+    this.subscriptionService = subscriptionService;
+  }
+
+  async getPaymentLink(req, res) {
+    try {
+
+      const articulo = req.query.articulo;
+      const correo = req.query.correo;
+      const cantidad = req.query.cantidad;
+      const precio = req.query.precio;
+
+      const payment = await this.subscriptionService.createPayment(articulo, correo, cantidad, precio);
+
+      return res.json(payment);
+    } catch (error) {
+      console.log(error);
+
+      return res
+        .status(500)
+        .json({ error: true, msg: "Failed to create payment" });
+    }
+  }
+
+  async getSubscriptionLink(req, res) {
+    try {
+      const subscription = await this.subscriptionService.createSubscription();
+
+      return res.json(subscription);
+    } catch (error) {
+      console.log(error);
+
+      return res
+        .status(500)
+        .json({ error: true, msg: "Failed to create subscription" });
+    }
+  }
+}
+
+module.exports = PaymentController;
---
+++ services/PaymentService.js
@@ -0,0 +1,63 @@
+const axios = require("axios");
+
+class PaymentService {
+  async createPayment(articulo, correo, cantidad, precio) {
+    const url = "https://api.mercadopago.com/checkout/preferences";
+
+    const body = {
+      payer_email: correo,
+      items: [
+        {
+          title: articulo,
+          description: articulo,
+          picture_url: "http://www.myapp.com/myimage.jpg",
+          category_id: "Lubricante",
+          quantity: Number(cantidad),
+          currency_id: "CLP",
+          unit_price: Number(precio) //precio unitario // Mercado pago convierte
+        }
+      ],
+      back_urls: {
+        failure: "/failure",
+        pending: "/pending",
+        success: "/success"
+      }
+    };
+
+    const payment = await axios.post(url, body, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
+      }
+    });
+
+    return payment.data;
+  }
+
+  async createSubscription() {
+    const url = "https://api.mercadopago.com/preapproval";
+
+    const body = {
+      reason: "Suscripción de ejemplo",
+      auto_recurring: {
+        frequency: 1,
+        frequency_type: "months",
+        transaction_amount: 10,
+        currency_id: "CLP"
+      },
+      back_url: "https://google.com",
+      payer_email: "test_user_22631142@testuser.com"
+    };
+
+    const subscription = await axios.post(url, body, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
+      }
+    });
+
+    return subscription.data;
+  }
+}
+
+module.exports = PaymentService;
